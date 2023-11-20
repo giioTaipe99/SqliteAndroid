84531e8f177f9e3d276646dc6c783dc74aaa509c
giioTaipe99 <giiovannytaipe88@gmail.com>
2023-11-20 23:44:06 +0100

    commit

--- app/src/main/java/com/example/myapplication/Add_Profile.java
+++ app/src/main/java/com/example/myapplication/Add_Profile.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Add_Profile extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Add_Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_profile);
+        // Obtén la referencia del Spinner desde el diseño
+        Spinner spinnerPhoneType = findViewById(R.id.spinnerPhoneType);
+
+        // Define las opciones del Spinner
+        String[] phoneTypes = {"Fijo", "Móvil", "Trabajo"};
+
+        // Crea un adaptador para el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, phoneTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Asigna el adaptador al Spinner
+        spinnerPhoneType.setAdapter(adapter);
     }
 }
